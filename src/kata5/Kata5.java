c609c81e306c4835981c0e4d2737946b13bae24b
Entrar <Entrar@PC-1111>
2021-11-18 11:57:39 +0000

    kata5 código ejemplo 1

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -1,21 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kata5;
 
-/**
- *
- * @author Entrar
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Kata5 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        connect();
+    }
+
+    private static void connect() {
+        Connection conn;
+        conn = null;
+        try {
+            String url;
+            url = "jdbc:sqlite:Kata5.db";
+            
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("Connexion a SQLite establecida");
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        } finally {
+            try{
+                if (conn != null){
+                    conn.close();
+                }
+            
+            } catch (SQLException ex){
+                System.out.println(ex.getMessage());
+            }
+        
+        }
     }
     
 }
